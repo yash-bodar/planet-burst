ac9f7d2b55ec24b13c38082e6507405d3846e1c2
Yash Bodar <yash@parextech.com>
2026-09-17 11:43:52 +0530

    update i[

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
         host: '0.0.0.0',
         cors: true,
         hmr: {
-            host: '192.168.1.180',
+            host: 'localhost',
         },
         watch: {
             ignored: ['**/storage/framework/views/**'],
